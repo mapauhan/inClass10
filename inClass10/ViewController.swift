b577d73512302900bfa02920f43d55cf448a5a28
Handschu, Maria <mhandsch@cci01ngg82aws.dyn.uncc.edu>
2019-04-03 19:11:34 -0400

    Initial Commit

---
+++ inClass10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inClass10
+//
+//  Created by Handschu, Maria on 4/3/19.
+//  Copyright © 2019 Handschu, Maria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
